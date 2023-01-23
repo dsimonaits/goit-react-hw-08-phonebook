69da8b185901d57456e116b629e985ce6e3e8595
Deniss Simonaits <dsimonaits@gmail.com>
2023-01-23 21:29:01 +0200

    Create ContactsPage.jsx

---
+++ src/pages/ContactsPage.jsx
@@ -0,0 +1,25 @@
+import { useDispatch, useSelector } from 'react-redux';
+import { useEffect } from 'react';
+import { selectUserRefreshCompleted } from 'redux/auth/auth-selectors';
+import { fetchContacts } from 'redux/contacts/operations';
+import 'react-toastify/dist/ReactToastify.css';
+import Section from 'components/Section/Section';
+import { ContactsWrapper } from 'components/Contacts/ContactsWrapper/ContactsWrapper';
+
+export const Contacts = () => {
+  const dispatch = useDispatch();
+  const refreshingComplete = useSelector(selectUserRefreshCompleted);
+
+  useEffect(() => {
+    if (!refreshingComplete) {
+      return;
+    }
+    dispatch(fetchContacts());
+  }, [dispatch, refreshingComplete]);
+
+  return (
+    <Section>
+      <ContactsWrapper />
+    </Section>
+  );
+};
