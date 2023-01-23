8fa08f15ebdb47023822ab066b04dcbaf47356ca
Deniss Simonaits <dsimonaits@gmail.com>
2023-01-23 21:28:45 +0200

    Create selectors.js

---
+++ src/redux/contacts/selectors.js
@@ -0,0 +1,14 @@
+import { createSelector } from '@reduxjs/toolkit';
+
+export const selectFilter = state => state.filter.filter;
+export const selectContacts = state => state.contacts.items;
+export const selectIsLoading = state => state.contacts.isLoading;
+export const selectError = state => state.contacts.error;
+
+export const selectFilteredContacts = createSelector(
+  [selectContacts, selectFilter],
+  (contacts, filter) =>
+    contacts.filter(({ name }) =>
+      name.toLowerCase().includes(filter.toLowerCase())
+    )
+);
