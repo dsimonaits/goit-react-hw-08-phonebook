3deae81deb1e4a86ab52750d6fe6d7cdb3456f2b
Deniss Simonaits <dsimonaits@gmail.com>
2023-01-23 21:29:26 +0200

    Create Container.styled.js

---
+++ src/components/Container/Container.styled.js
@@ -0,0 +1,29 @@
+import styled from 'styled-components';
+
+export const ContainerStyled = styled.div`
+  padding-left: 20px;
+  padding-right: 20px;
+
+  &.hero {
+    display: flex;
+    text-align: center;
+    flex-direction: column;
+    align-items: center;
+    gap: 10px;
+  }
+
+  min-height: ${props => props.height};
+  @media screen and (min-width: 320px) {
+    width: 320px;
+    margin-right: auto;
+    margin-left: auto;
+  }
+
+  @media screen and (min-width: 768px) {
+    width: 768px;
+  }
+
+  @media screen and (min-width: 1200px) {
+    width: 1200px;
+  }
+`;
