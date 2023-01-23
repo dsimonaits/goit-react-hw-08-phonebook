78e8db3f6e1cde3de71b4a1538192e8b86a00d4e
Deniss Simonaits <dsimonaits@gmail.com>
2023-01-23 22:31:42 +0200

    Update AuthMenu.jsx

--- src/components/AuthMenu/AuthMenu.jsx
+++ src/components/AuthMenu/AuthMenu.jsx
@@ -1,15 +1,23 @@
 import { useDispatch, useSelector } from 'react-redux';
+import { useState } from 'react';
 import { logout } from 'redux/auth/auth-operations';
 import { selectUser } from 'redux/auth/auth-selectors';
-import { AuthMenuStyled } from './AuthMenu.styled';
-
+import { AuthMenuStyled, BurgerButton } from './AuthMenu.styled';
+import { GiHamburgerMenu } from 'react-icons/gi';
 export const AuthMenu = () => {
+  const [showModal, setShowModal] = useState(false);
+
   const user = useSelector(selectUser);
   const dispatch = useDispatch();
   return (
-    <AuthMenuStyled>
-      <p>{`Welcome, ${user}!`}</p>
-      <button onClick={() => dispatch(logout())}>LogOut</button>
-    </AuthMenuStyled>
+    <>
+      <AuthMenuStyled>
+        <p>{`Welcome, ${user}!`}</p>
+        <button onClick={() => dispatch(logout())}>LogOut</button>
+      </AuthMenuStyled>
+      <BurgerButton onClick={() => setShowModal(true)}>
+        <GiHamburgerMenu size="1.5em" />
+      </BurgerButton>
+    </>
   );
 };
