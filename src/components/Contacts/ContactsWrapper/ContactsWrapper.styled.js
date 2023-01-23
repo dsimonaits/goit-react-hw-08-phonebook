b0719c56539deafc99df6f160cd49a5fe69aa8fb
Deniss Simonaits <dsimonaits@gmail.com>
2023-01-23 21:29:34 +0200

    Create ContactsWrapper.styled.js

---
+++ src/components/Contacts/ContactsWrapper/ContactsWrapper.styled.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+import Container from 'components/Container/Container';
+
+export const Wrapper = styled(Container)`
+  padding-top: 10px;
+  padding-bottom: 10px;
+  display: flex;
+  justify-content: center;
+  gap: 5px;
+`;
+
+export const LeftBar = styled.div``;
+
+export const RightBar = styled.div``;
