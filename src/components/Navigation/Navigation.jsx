da2b32cf9cba588748281be2538c6d0c6c450494
Deniss Simonaits <dsimonaits@gmail.com>
2023-01-23 21:29:16 +0200

    Create Navigation.jsx

---
+++ src/components/Navigation/Navigation.jsx
@@ -0,0 +1,29 @@
+import { AuthMenu } from 'components/AuthMenu/AuthMenu';
+import { AuthNav } from 'components/AuthNav/AuthNav';
+import { useSelector } from 'react-redux';
+import { selectToken } from 'redux/auth/auth-selectors';
+import { MainNav, SiteNav, NavLinkStyled } from './Navigation.styled';
+
+export const Navigation = () => {
+  const token = useSelector(selectToken);
+  return (
+    <MainNav>
+      <span>
+        <b>PhoneBook</b>
+      </span>
+      <nav>
+        <SiteNav>
+          <li>
+            <NavLinkStyled to="/">Home</NavLinkStyled>
+          </li>
+          {token && (
+            <li>
+              <NavLinkStyled to="contacts">Contacts</NavLinkStyled>
+            </li>
+          )}
+        </SiteNav>
+      </nav>
+      {token ? <AuthMenu /> : <AuthNav />}
+    </MainNav>
+  );
+};
