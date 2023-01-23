c2aa433042136c8ac2a23e12643e5f1a27d9e662
Deniss Simonaits <dsimonaits@gmail.com>
2023-01-23 21:38:17 +0200

    Update AuthNav.styled.js

--- src/components/AuthNav/AuthNav.styled.js
+++ src/components/AuthNav/AuthNav.styled.js
@@ -1,6 +1,13 @@
 import styled from 'styled-components';
+import { NavLink } from 'react-router-dom';
 
 export const AuthNavstyled = styled.ul`
   display: flex;
   gap: 10px;
+`;
+
+export const NavLinkStyled = styled(NavLink)`
+  &.active {
+    color: red;
+  }
 `;
