1d95f58bf191b934470c45845524d74253bbb45f
Deniss Simonaits <dsimonaits@gmail.com>
2023-01-23 22:31:40 +0200

    Update AuthMenu.styled.js

--- src/components/AuthMenu/AuthMenu.styled.js
+++ src/components/AuthMenu/AuthMenu.styled.js
@@ -3,4 +3,20 @@ import styled from 'styled-components';
 export const AuthMenuStyled = styled.div`
   display: flex;
   gap: 10px;
+
+  @media screen and (max-width: 768px) {
+    display: none;
+  }
+`;
+
+export const BurgerButton = styled.button`
+  display: none;
+  border: none;
+  background-color: transparent;
+
+  cursor: pointer;
+
+  @media screen and (max-width: 768px) {
+    display: block;
+  }
 `;
