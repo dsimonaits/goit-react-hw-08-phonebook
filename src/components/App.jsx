148dfe36d574e95d0c2e5af84762f15fff91aef6
Deniss Simonaits <dsimonaits@gmail.com>
2023-01-23 21:30:06 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,65 @@
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+// import { selectUserRefresh } from 'redux/auth/auth-selectors';
+import { selectUserRefreshCompleted } from 'redux/auth/auth-selectors';
+import { refreshUser } from 'redux/auth/auth-operations';
+import { HomePage } from 'pages/HomePage';
+import { Routes, Route } from 'react-router-dom';
+import { Contacts } from 'pages/ContactsPage';
+import { Layout } from './Layout/Layout';
+import { RegisterPage } from 'pages/RegisterPage';
+import { LoginPage } from 'pages/LoginPage';
+import { PrivateRoute } from './PrivateRoute';
+import { PublicRoute } from './PublicRoute';
+
 export const App = () => {
+  const dispatch = useDispatch();
+  const refreshingCompleted = useSelector(selectUserRefreshCompleted);
+
+  useEffect(() => {
+    dispatch(refreshUser());
+  }, [dispatch]);
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <>
+      {refreshingCompleted && (
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route
+              index
+              element={
+                <PublicRoute>
+                  <HomePage />
+                </PublicRoute>
+              }
+            />
+            <Route
+              path="contacts"
+              element={
+                <PrivateRoute redirectTo="/login">
+                  <Contacts />
+                </PrivateRoute>
+              }
+            />
+            <Route
+              path="register"
+              element={
+                <PublicRoute restricted redirectTo="/contacts">
+                  <RegisterPage />
+                </PublicRoute>
+              }
+            />
+            <Route
+              path="login"
+              element={
+                <PublicRoute restricted redirectTo="/contacts">
+                  <LoginPage />
+                </PublicRoute>
+              }
+            />
+          </Route>
+        </Routes>
+      )}
+    </>
   );
 };
