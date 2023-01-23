e766d8eccae4d8e6ded0a4c2869cde54aceb6453
Deniss Simonaits <dsimonaits@gmail.com>
2023-01-23 21:29:14 +0200

    Create Navigation.styled.js

---
+++ src/components/Navigation/Navigation.styled.js
@@ -0,0 +1,22 @@
+import styled from 'styled-components';
+import Container from 'components/Container/Container';
+import { NavLink } from 'react-router-dom';
+
+export const MainNav = styled(Container)`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding-bottom: 5px;
+  border-bottom: solid 1px black;
+`;
+
+export const SiteNav = styled.ul`
+  display: flex;
+  gap: 10px;
+`;
+
+export const NavLinkStyled = styled(NavLink)`
+  &.active {
+    color: red;
+  }
+`;
