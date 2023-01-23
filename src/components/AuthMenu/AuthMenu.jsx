db47931e77b6ed973c1d4c5286b8269d2e385258
Deniss Simonaits <dsimonaits@gmail.com>
2023-01-24 00:19:31 +0200

    Update AuthMenu.jsx

--- src/components/AuthMenu/AuthMenu.jsx
+++ src/components/AuthMenu/AuthMenu.jsx
@@ -4,20 +4,39 @@ import { logout } from 'redux/auth/auth-operations';
 import { selectUser } from 'redux/auth/auth-selectors';
 import { AuthMenuStyled, BurgerButton } from './AuthMenu.styled';
 import { GiHamburgerMenu } from 'react-icons/gi';
+import ModalWindow from 'components/Modal/Modal';
 export const AuthMenu = () => {
   const [showModal, setShowModal] = useState(false);
 
   const user = useSelector(selectUser);
   const dispatch = useDispatch();
-  return (
-    <>
-      <AuthMenuStyled>
+
+  const openModal = () => {
+    setShowModal(true);
+  };
+
+  const closeModal = () => {
+    setShowModal(false);
+  };
+
+  const AuthContent = () => {
+    return (
+      <>
         <p>{`Welcome, ${user}!`}</p>
         <button onClick={() => dispatch(logout())}>LogOut</button>
-      </AuthMenuStyled>
-      <BurgerButton onClick={() => setShowModal(true)}>
+      </>
+    );
+  };
+
+  return (
+    <>
+      <AuthMenuStyled>{AuthContent()}</AuthMenuStyled>
+      <BurgerButton onClick={openModal}>
         <GiHamburgerMenu size="1.5em" />
       </BurgerButton>
+      {showModal && (
+        <ModalWindow onClick={closeModal}>{AuthContent()}</ModalWindow>
+      )}
     </>
   );
 };
