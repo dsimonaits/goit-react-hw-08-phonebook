52a737e1b78e977921eb8bedab9ab9ca3739dc03
Deniss Simonaits <dsimonaits@gmail.com>
2023-01-23 21:28:51 +0200

    Create contactsSlice.js

---
+++ src/redux/contacts/contactsSlice.js
@@ -0,0 +1,60 @@
+import { createSlice, isAnyOf } from '@reduxjs/toolkit';
+import { fetchContacts, deleteContact, addContact } from './operations';
+
+const initialState = {
+  items: [],
+  isLoading: false,
+  error: null,
+};
+
+const contactsSlice = createSlice({
+  name: 'contacts',
+  initialState,
+  extraReducers: builder =>
+    builder
+      .addCase(fetchContacts.fulfilled, (state, { payload }) => {
+        state.items = payload;
+      })
+      .addCase(deleteContact.fulfilled, (state, { payload }) => {
+        state.items = state.items.filter(({ id }) => id !== payload);
+      })
+      .addCase(addContact.fulfilled, (state, { payload }) => {
+        state.items = [...state.items, payload];
+      })
+      .addMatcher(
+        isAnyOf(
+          fetchContacts.pending,
+          deleteContact.pending,
+          addContact.pending
+        ),
+        state => {
+          state.isLoading = true;
+        }
+      )
+      .addMatcher(
+        isAnyOf(
+          fetchContacts.fulfilled,
+          deleteContact.fulfilled,
+          addContact.fulfilled
+        ),
+        state => {
+          state.isLoading = false;
+          state.error = null;
+        }
+      )
+      .addMatcher(
+        isAnyOf(
+          fetchContacts.rejected,
+          deleteContact.rejected,
+          addContact.rejected
+        ),
+        (state, { payload }) => {
+          state.isLoading = false;
+          state.error = payload;
+        }
+      ),
+});
+
+export const data = contactsSlice.actions;
+
+export default contactsSlice.reducer;
