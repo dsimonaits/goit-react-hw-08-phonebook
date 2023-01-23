2851aedbdab20ce01a3684df247e7a10d8c2307c
Deniss Simonaits <dsimonaits@gmail.com>
2023-01-23 21:29:31 +0200

    Create Filter.styled.js

---
+++ src/components/Contacts/Filter/Filter.styled.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const Label = styled.label`
+  /* width: 300px; */
+  display: flex;
+  flex-flow: column;
+  align-items: center;
+  justify-content: center;
+  gap: 5px;
+`;
