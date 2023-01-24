e40956d721cdc8d50b2fe61d4a627650745b72e9
Deniss Simonaits <dsimonaits@gmail.com>
2023-01-25 01:24:14 +0200

    Update ButtonMui.styled.js

--- src/components/ButtonMui/ButtonMui.styled.js
+++ src/components/ButtonMui/ButtonMui.styled.js
@@ -2,6 +2,6 @@ import styled from 'styled-components';
 import Button from '@mui/material/Button';
 
 export const ButtonMuiStyled = styled(Button)`
-  width: 130px;
+  width: auto;
   height: 30px;
 `;
