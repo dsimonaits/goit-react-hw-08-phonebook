9ea77da93f46dca1bdf4d527740e071fff560613
Deniss Simonaits <dsimonaits@gmail.com>
2023-01-24 22:17:43 +0200

    Update ContactForm.styled.js

--- src/components/Contacts/ContactForm/ContactForm.styled.js
+++ src/components/Contacts/ContactForm/ContactForm.styled.js
@@ -13,14 +13,6 @@ export const Form = styled.form`
   border: 1px solid black;
   border-radius: 5px;
   padding: 10px;
-
-  @media screen and (min-width: 768px) {
-    width: 200px;
-  }
-
-  @media screen and (min-width: 1200px) {
-    width: 200px;
-  }
 `;
 
 export const Label = styled.label`
@@ -31,11 +23,4 @@ export const Label = styled.label`
 
 export const Input = styled.input`
   width: 120px;
-  @media screen and (min-width: 768px) {
-    width: 200px;
-  }
-
-  @media screen and (min-width: 1200px) {
-    width: 200px;
-  }
 `;
