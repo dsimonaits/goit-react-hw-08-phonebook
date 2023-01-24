1bcde253c13b801f9faf19f9b0a282b058b98b41
Deniss Simonaits <dsimonaits@gmail.com>
2023-01-24 21:09:35 +0200

    Update AuthMenu.styled.js

--- src/components/AuthMenu/AuthMenu.styled.js
+++ src/components/AuthMenu/AuthMenu.styled.js
@@ -1,20 +1,21 @@
 import styled from 'styled-components';
 
 export const AuthMenuStyled = styled.div`
-  @media screen and (min-width: 768px) {
+  display: none;
+  /* display: flex;
+  gap: 10px; */
+
+  @media screen and (min-width: 767.9px) {
     display: flex;
     gap: 10px;
   }
 `;
 
 export const BurgerButton = styled.button`
-  display: none;
   border: none;
   background-color: transparent;
-
   cursor: pointer;
-
-  @media screen and (max-width: 768px) {
-    display: block;
+  @media screen and (min-width: 767.9px) {
+    display: none;
   }
 `;
