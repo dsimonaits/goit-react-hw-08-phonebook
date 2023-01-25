62c725ffac3c7660dafaa68c57684e8d34a1a2c1
Deniss Simonaits <dsimonaits@gmail.com>
2023-01-25 22:32:32 +0200

    Update AuthNav.styled.js

--- src/components/AuthNav/AuthNav.styled.js
+++ src/components/AuthNav/AuthNav.styled.js
@@ -1,13 +1,39 @@
 import styled from 'styled-components';
 import { NavLink } from 'react-router-dom';
 
-export const AuthNavstyled = styled.ul`
+export const AuthNavStyled = styled.ul`
+  display: none;
+
+  @media screen and (min-width: 767.9px) {
+    display: flex;
+
+    gap: 10px;
+  }
+`;
+
+export const ModalAuthContent = styled.ul`
   display: flex;
+  flex-direction: column;
   gap: 10px;
 `;
 
 export const NavLinkStyled = styled(NavLink)`
   &.active {
     color: #1976d2;
+  }
+`;
+
+export const AuthContentStyled = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 10px;
+`;
+
+export const BurgerButton = styled.button`
+  border: none;
+  background-color: transparent;
+  cursor: pointer;
+  @media screen and (min-width: 767.9px) {
+    display: none;
   }
 `;
