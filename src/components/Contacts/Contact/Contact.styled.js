8a673d270c700748fbe43553800bc0fb642f509e
Deniss Simonaits <dsimonaits@gmail.com>
2023-01-26 01:40:34 +0200

    Update Contact.styled.js

--- src/components/Contacts/Contact/Contact.styled.js
+++ src/components/Contacts/Contact/Contact.styled.js
@@ -22,3 +22,29 @@ export const ContactWrapper = styled.div`
 export const B = styled.b`
   color: black;
 `;
+
+export const Form = styled.form`
+  padding-top: 20px;
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+  align-items: center;
+  /* justify-content: center; */
+
+  color: #373434a6;
+  background-color: white;
+
+  border: 1px solid black;
+  border-radius: 5px;
+  padding: 10px;
+`;
+
+export const Label = styled.label`
+  display: flex;
+  gap: 5px;
+  flex-flow: column;
+`;
+
+export const Input = styled.input`
+  width: 120px;
+`;
