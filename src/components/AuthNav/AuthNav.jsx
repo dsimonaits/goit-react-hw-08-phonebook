94eb004e56d2e94596080aed7e8aa79c623a2f26
Deniss Simonaits <dsimonaits@gmail.com>
2023-01-25 22:32:34 +0200

    Update AuthNav.jsx

--- src/components/AuthNav/AuthNav.jsx
+++ src/components/AuthNav/AuthNav.jsx
@@ -1,14 +1,53 @@
-import { AuthNavstyled, NavLinkStyled } from './AuthNav.styled';
+import { useState } from 'react';
+import { GiHamburgerMenu } from 'react-icons/gi';
+import ModalWindow from 'components/Modal/Modal';
+import {
+  AuthNavStyled,
+  NavLinkStyled,
+  BurgerButton,
+  ModalAuthContent,
+} from './AuthNav.styled';
 
 export const AuthNav = () => {
+  const [showModal, setShowModal] = useState(false);
+
+  const openModal = () => {
+    setShowModal(true);
+  };
+
+  const closeModal = () => {
+    setShowModal(false);
+  };
+
+  const AuthContent = () => {
+    return (
+      <ModalAuthContent>
+        <li>
+          <NavLinkStyled to="register">Register</NavLinkStyled>
+        </li>
+        <li>
+          <NavLinkStyled to="login">Log In</NavLinkStyled>
+        </li>
+      </ModalAuthContent>
+    );
+  };
+
   return (
-    <AuthNavstyled>
-      <li>
-        <NavLinkStyled to="register">Register</NavLinkStyled>
-      </li>
-      <li>
-        <NavLinkStyled to="login">Log In</NavLinkStyled>
-      </li>
-    </AuthNavstyled>
+    <>
+      <AuthNavStyled>
+        <li>
+          <NavLinkStyled to="register">Register</NavLinkStyled>
+        </li>
+        <li>
+          <NavLinkStyled to="login">Log In</NavLinkStyled>
+        </li>
+      </AuthNavStyled>
+      <BurgerButton onClick={openModal}>
+        <GiHamburgerMenu size="1.5em" />
+      </BurgerButton>
+      {showModal && (
+        <ModalWindow onClick={closeModal}>{AuthContent()}</ModalWindow>
+      )}
+    </>
   );
 };
