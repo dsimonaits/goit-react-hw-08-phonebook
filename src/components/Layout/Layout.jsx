a02dfc94676bdcbbc59985973ec4b63a7e85f5a2
Deniss Simonaits <dsimonaits@gmail.com>
2023-01-25 23:16:37 +0200

    Update Layout.jsx

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,22 +1,48 @@
 import { Suspense } from 'react';
 import { Outlet } from 'react-router-dom';
 import { Navigation } from 'components/Navigation/Navigation';
+import { useEffect } from 'react';
+import { useSelector } from 'react-redux';
 import { Header } from './Layout.styled';
 import Footer from 'components/Footer/Footer';
 import { PageContainer } from './Layout.styled';
+import { ToastContainer, toast } from 'react-toastify';
+import { selectAuthError } from 'redux/auth/auth-selectors';
 
 export const Layout = () => {
+  // const contactsError = useSelector(selectContactsError);
+  const authError = useSelector(selectAuthError);
+  console.log(authError);
+
+  useEffect(() => {
+    switch (authError) {
+      case 'Network Error':
+        toast('Network Error');
+        break;
+      case 'Please authenticate':
+        toast('Your session has been expired please login again');
+        break;
+      // case value:
+      //   break;
+      default:
+        break;
+    }
+  }, [authError]);
+
   return (
-    <PageContainer>
-      <Header>
-        <Navigation />
-      </Header>
-      <main>
-        <Suspense fallback={<p>Loading...</p>}>
-          <Outlet />
-        </Suspense>
-      </main>
-      <Footer />
-    </PageContainer>
+    <>
+      <PageContainer>
+        <Header>
+          <Navigation />
+        </Header>
+        <main>
+          <Suspense fallback={<p>Loading...</p>}>
+            <Outlet />
+          </Suspense>
+        </main>
+        <Footer />
+      </PageContainer>
+      <ToastContainer />
+    </>
   );
 };
