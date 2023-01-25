f382b5029be40e9dc1380f0f7df7064b07e83d1e
Deniss Simonaits <dsimonaits@gmail.com>
2023-01-25 22:32:22 +0200

    Update Layout.styled.js

--- src/components/Layout/Layout.styled.js
+++ src/components/Layout/Layout.styled.js
@@ -1,5 +1,11 @@
 import styled from 'styled-components';
+import Container from 'components/Container/Container';
 
 export const Header = styled.header`
+  position: relative;
   padding-top: 10px;
+`;
+
+export const PageContainer = styled(Container)`
+  padding: 0px;
 `;
