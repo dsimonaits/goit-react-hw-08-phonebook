12bbda74172f870f9ecb7bf6339f34f423034128
Deniss Simonaits <dsimonaits@gmail.com>
2023-01-25 22:32:25 +0200

    Create Footer.styled.js

---
+++ src/components/Footer/Footer.styled.js
@@ -0,0 +1,28 @@
+import styled from 'styled-components';
+import Container from 'components/Container/Container';
+
+export const FooterStyled = styled.footer`
+  position: absolute;
+  bottom: 20px;
+  width: auto;
+  height: auto;
+`;
+
+export const FooterContainer = styled(Container)`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding-top: 5px;
+  gap: 5px;
+
+  border-top: 1px solid black;
+`;
+
+export const Link = styled.a`
+  display: flex;
+  gap: 5px;
+  align-items: center;
+  &:hover {
+    color: var(--secondary-color);
+  }
+`;
