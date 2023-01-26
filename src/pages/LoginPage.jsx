d4787c79b766cefafddbc5885648e0c2e00e1c60
Deniss Simonaits <dsimonaits@gmail.com>
2023-01-26 20:00:20 +0200

    Update LoginPage.jsx

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,10 +1,14 @@
 // import { LoginForm } from 'components/LoginForm/LoginForm';
 import SignIn from 'components/LoginFormMui/LoginFormMui';
+import Container from 'components/Container/Container';
+import Section from 'components/Section/Section';
 
 export const LoginPage = () => {
   return (
-    <>
-      <SignIn />
-    </>
+    <Section>
+      <Container>
+        <SignIn />
+      </Container>
+    </Section>
   );
 };
