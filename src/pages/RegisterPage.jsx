9c9bfe32e06e64b285d7e51ebc5163a84b203151
Deniss Simonaits <dsimonaits@gmail.com>
2023-01-26 20:00:18 +0200

    Update RegisterPage.jsx

--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -1,6 +1,14 @@
 // import { RegisterForm } from 'components/RegisterForm/RegisterForm';
 import Register from 'components/RegisterFormMui/RegisterFormMui';
+import Section from 'components/Section/Section';
+import Container from 'components/Container/Container';
 
 export const RegisterPage = () => {
-  return <Register />;
+  return (
+    <Section>
+      <Container>
+        <Register />
+      </Container>
+    </Section>
+  );
 };
