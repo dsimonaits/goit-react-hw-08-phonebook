0b3252c4c6327be74b145681bd2dccd1460c9e8d
Deniss Simonaits <dsimonaits@gmail.com>
2023-01-26 23:32:59 +0200

    Update auth-selectors.js

--- src/redux/auth/auth-selectors.js
+++ src/redux/auth/auth-selectors.js
@@ -2,5 +2,6 @@ export const selectToken = state => state.auth.token;
 export const selectUser = state => state.auth.user.name;
 export const selectEmail = state => state.auth.user.email;
 export const selectAuthError = state => state.auth.error;
+export const selectAuthLoading = state => state.auth.isLoading;
 export const selectUserRefreshCompleted = state =>
   state.auth.userRefreshCompleted;
