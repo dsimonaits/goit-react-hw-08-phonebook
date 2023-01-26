75553a8d03033ce7658bf874480c7ade6b673ea5
Deniss Simonaits <dsimonaits@gmail.com>
2023-01-27 01:15:01 +0200

    Update Contact.styled.js

--- src/components/Contacts/Contact/Contact.styled.js
+++ src/components/Contacts/Contact/Contact.styled.js
@@ -1,22 +1,14 @@
 import styled from 'styled-components';
 
 export const ContactWrapper = styled.div`
+  padding: 5px;
   display: flex;
-  flex-direction: column;
-  align-items: left;
+  flex-flow: column;
+  justify-content: space-between;
+
   color: #373434a6;
-  gap: 5px;
-  width: 120px;
-
-  line-height: 1.3;
-
-  @media screen and (min-width: 768px) {
-    width: 300px;
-  }
-
-  @media screen and (min-width: 1200px) {
-    width: 300px;
-  }
+  margin: auto;
+  min-width: 260px;
 `;
 
 export const B = styled.b`
@@ -29,7 +21,6 @@ export const Form = styled.form`
   flex-direction: column;
   gap: 10px;
   align-items: center;
-  /* justify-content: center; */
 
   color: #373434a6;
   background-color: white;
@@ -37,6 +28,10 @@ export const Form = styled.form`
   border: 1px solid black;
   border-radius: 5px;
   padding: 10px;
+
+  @media screen and (min-width: 768px) {
+    flex-direction: row;
+  } ;
 `;
 
 export const Label = styled.label`
@@ -50,6 +45,14 @@ export const Input = styled.input`
 `;
 
 export const ButtonWrapper = styled.div`
+  padding-top: 10px;
   display: flex;
+  margin: auto;
   gap: 10px;
+`;
+
+export const DataWrapper = styled.div`
+  min-width: 180px;
+  display: flex;
+  justify-content: space-between;
 `;
