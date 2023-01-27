09057887685d898e27935c76cf0f09c7b020853e
Deniss Simonaits <dsimonaits@gmail.com>
2023-01-27 02:07:50 +0200

    Update ContactsWrapper.jsx

--- src/components/Contacts/ContactsWrapper/ContactsWrapper.jsx
+++ src/components/Contacts/ContactsWrapper/ContactsWrapper.jsx
@@ -2,15 +2,14 @@ import ContactForm from '../ContactForm';
 import FilterByName from '../Filter/Filter';
 import ContactList from '../ContactList';
 import { LeftBar, RightBar, Wrapper } from './ContactsWrapper.styled';
-import Container from 'components/Container/Container';
 
 export const ContactsWrapper = () => {
   return (
     <>
-      <Container>
+      <Wrapper>
         <ContactForm />
         <FilterByName />
-      </Container>
+      </Wrapper>
 
       <Wrapper>
         <LeftBar>
